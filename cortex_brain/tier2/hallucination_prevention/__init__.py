f0c8c20eb5c6117bab149d2a3710a3b806feaac2
Asif Hussain <asifhussain60@users.noreply.github.com>
2026-01-20 17:24:13 -0500

    Phase D: 27 to 7 errors - BehavioralBoundaryRules export added

--- cortex_brain/tier2/hallucination_prevention/__init__.py
+++ cortex_brain/tier2/hallucination_prevention/__init__.py
@@ -1,1 +1,9 @@
-"""Auto-generated stub module."""
+"""Tier2 Hallucination Prevention Module.
+
+Author: CORTEX Framework
+Copyright © 2025-2026 Asif Hussain. All rights reserved.
+"""
+
+from cortex_brain.tier2.hallucination_prevention.boundary_rules import BehavioralBoundaryRules
+
+__all__ = ["BehavioralBoundaryRules"]
